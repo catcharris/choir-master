ab717fd79f00b1058b16138ffa61cbf86ce8e44a
Lemonpapa <lemonpapa@LemonpapaMacBook.local>
2026-02-16 03:59:14 +0900

    Disable PWA to fix build error on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
 import type { NextConfig } from "next";
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
-
 const nextConfig: NextConfig = {
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
+  /* config options here */
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;

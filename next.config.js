14560f23496985a62e9cdb2f89e7e1fac89cccb4
Lemonpapa <lemonpapa@LemonpapaMacBook.local>
2026-02-16 11:18:07 +0900

    Switch to next.config.js and ensure dependencies are locked

---
+++ next.config.js
@@ -0,0 +1,28 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    experimental: {
+        // disable turbo for now to ensure compatibility
+    }
+};
+
+module.exports = withPWA(nextConfig);
